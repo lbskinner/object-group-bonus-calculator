07b2b095ac1873d025b56ca0593b0640e09c003c
Skyler Burgard <skylerrburgard@hotmail.com>
2020-03-10 16:35:00 -0500

    First commit

--- client.js
+++ client.js
@@ -1,37 +1,60 @@
 const employees = [
   {
-    name: 'Atticus',
-    employeeNumber: '2405',
-    annualSalary: '47000',
+    name: "Atticus",
+    employeeNumber: "2405",
+    annualSalary: "47000",
     reviewRating: 3
   },
   {
-    name: 'Jem',
-    employeeNumber: '62347',
-    annualSalary: '63500',
+    name: "Jem",
+    employeeNumber: "62347",
+    annualSalary: "63500",
     reviewRating: 4
   },
   {
-    name: 'Scout',
-    employeeNumber: '6243',
-    annualSalary: '74750',
+    name: "Scout",
+    employeeNumber: "6243",
+    annualSalary: "74750",
     reviewRating: 5
   },
   {
-    name: 'Robert',
-    employeeNumber: '26835',
-    annualSalary: '66000',
+    name: "Robert",
+    employeeNumber: "26835",
+    annualSalary: "66000",
     reviewRating: 1
   },
   {
-    name: 'Mayella',
-    employeeNumber: '89068',
-    annualSalary: '35000',
+    name: "Mayella",
+    employeeNumber: "89068",
+    annualSalary: "35000",
     reviewRating: 1
   }
 ];
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
+
+function bonusCalculator(employee) {
+  let bonusPercent = 0;
+  if (employee.reviewRating == 3) {
+    bonusPercent = 0.04;
+  } else if (employee.reviewRating == 4) {
+    bonusPercent = 0.06;
+  } else if (employee.reviewRating == 5) {
+    bonusPercent = 0.1;
+  } else {
+    bonusPercent = 0;
+  }
+  if (employee.employeeNumber.length == 4) {
+    bonusPercent += 0.05;
+  }
+  if (employee.annualSalary > 65000 && bonusPercent > 0) {
+    bonusPercent -= 0.01;
+  }
+  if (bonusPercent > 0.13) {
+    bonusPercent = 0.13;
+  }
+  return bonusPercent;
+}
 
 // Take small steps! Don't write a for loop and two functions that do all of the calculations right away.
 // This problem is massive! Break the problem down. Use the debugger.
@@ -40,4 +63,4 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+console.log(employees);
